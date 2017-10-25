38ae55a4049688cd4d19d266c88a6774cdb0e5c8
anhnt81 <anhnt30194@gmail.com>
2017-10-25 15:14:24 +0700

    Create module for all requirement

---
+++ app/code/local/AnhNT9/Slider/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanAnh
+ * Date: 10/25/2017
+ * Time: 3:09 PM
+ */
+class AnhNT9_Slider_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo 'Hello World Slider';
+    }
+}
