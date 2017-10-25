cb2684301183350268d8a77fed762b8168fbaf57
Admin <dothanhdatkhmt@gmail.com>
2017-10-25 15:47:41 +0700

    test

---
+++ app/code/local/Datdt/Assignment4/controllers/IndexController.php
@@ -0,0 +1,53 @@
+<?php
+
+class Datdt_Assignment4_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+
+         echo "ahihi";
+        // $layoutObject = $this->getLayout();
+
+        // $logo = $layoutObject->createBlock('core/template', 'logo')
+        //     ->setTemplate('assignment4/logo.phtml');
+
+        // $blocka = $layoutObject->createBlock('core/template', 'blocka')
+        //     ->setTemplate('assignment4/blocka.phtml');
+
+        // $blockb = $layoutObject->createBlock('core/template', 'blockb')
+        //     ->setTemplate('assignment4/blockb.phtml');
+
+        // $blockc = $layoutObject->createBlock('core/template', 'blockc')
+        //     ->setTemplate('assignment4/blockc.phtml');
+
+        // $blockd = $layoutObject->createBlock('core/template', 'blockd')
+        //     ->setTemplate('assignment4/blockd.phtml');
+
+        // $header = $layoutObject->createBlock('core/template', 'header')
+        //     ->setTemplate('assignment4/header.phtml')
+        //     ->append($logo, 'logo');
+
+        // $left = $layoutObject->createBlock('core/text_list', 'left')
+        // ->append($blockc, 'blockc');
+
+        // $content = $layoutObject->createBlock('core/text_list', 'content')
+        //     ->append($blocka, 'blocka')
+        //     ->append($blockb, 'blockb');
+
+        // $right = $layoutObject->createBlock('core/text_list', 'right')
+        //     ->append($blockd, 'blockd');
+
+        // $footer = $layoutObject->createBlock('core/template', 'footer')
+        //     ->setTemplate('assignment4/footer.phtml');
+
+        // $block = $layoutObject->createBlock('core/template', 'mainblock')
+        //     ->setTemplate('assignment4/mainblock.phtml')
+        //     ->append($header, 'header')
+        //     ->append($left, 'left')
+        //     ->append($content, 'content')
+        //     ->append($right, 'right')
+        //     ->append($footer, 'footer');
+
+        // echo $block->toHtml();
+    }
+}
