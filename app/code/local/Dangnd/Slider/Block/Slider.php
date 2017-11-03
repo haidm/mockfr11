e81a24395bd5635fb7d865346b04f3890ebff364
dangnd <nddang196@gmail.com>
2017-11-03 11:44:43 +0700

    Update module Slider  - add front-end : error multi image in one slide

--- app/code/local/Dangnd/Slider/Block/Slider.php
+++ app/code/local/Dangnd/Slider/Block/Slider.php
@@ -6,7 +6,36 @@
  * Time: 10:02 SA
  */
 
-class Dangnd_Slider_Block_Slider extends Mage_Core_Block_Abstract
+class Dangnd_Slider_Block_Slider extends Mage_Core_Block_Template
 {
+    public function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('myslider/index.phtml')
+            ->setData('list', $this->getSlider());
+    }
 
+    public function getSlider()
+    {
+        $listSlide = Mage::getModel('dangnd_slider/slide')
+            ->getCollection()
+            ->toArray();
+        $listSlide = $listSlide['items'];
+
+        foreach ($listSlide as $key => $item) {
+            $listSlide[$key]['images'] = $this->getImages($item['id']);
+        }
+
+        return $listSlide;
+    }
+
+    public function getImages($slideId)
+    {
+        $images = Mage::getModel('dangnd_slider/images')
+            ->getCollection()
+            ->addFilter('slideId', $slideId)
+            ->toArray();
+
+        return $images['items'];
+    }
 }
