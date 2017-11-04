77133287fe5a44f4f87fe05e2bcc3aca27de0bad
dangnd <nddang196@gmail.com>
2017-11-04 17:51:04 +0700

    Update module Bestseller  - Create configure system admin

---
+++ app/code/local/Dangnd/Bestseller/Model/EffectSlide.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: nddang
+ * Date: 01-11-2017
+ * Time: 3:01 CH
+ */
+
+class Dangnd_Bestseller_Model_EffectSlide
+{
+    public function toOptionArray()
+    {
+        return [
+            [
+                'label' => 'Fade',
+                'value' => 'fade'
+            ],
+            [
+                'label' => 'Cube',
+                'value' => 'cube'
+            ],
+            [
+                'label' => 'Flip',
+                'value' => 'flip'
+            ],
+            [
+                'label' => 'Coverflow',
+                'value' => 'coverflow'
+            ]
+        ];
+    }
+}
