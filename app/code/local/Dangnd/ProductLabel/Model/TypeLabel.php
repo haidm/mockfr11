769545ec0da006f5f41b154aa0c296123f113416
dangnd <nddang196@gmail.com>
2017-11-08 10:07:27 +0700

    Update module Product Label - Create attribute product_label with install script

---
+++ app/code/local/Dangnd/ProductLabel/Model/TypeLabel.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * User: nddang
+ * Date: 01-11-2017
+ * Time: 3:01 CH
+ */
+
+class Dangnd_ProductLabel_Model_TypeLabel extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
+{
+    public function toOptionArray()
+    {
+        $options = array();
+
+        foreach ($this->getAllOptions() as $option) {
+            $options[$option["value"]] = $option["label"];
+        }
+        return $options;
+    }
+
+    /**
+     * Retrieve All options
+     *
+     * @return array
+     */
+    public function getAllOptions()
+    {
+        if (is_null($this->_options)) {
+            $this->_options = array(
+
+                array(
+                    "label" => Mage::helper("eav")->__("New"),
+                    "value" =>  'new'
+                ),
+                array(
+                    "label" => Mage::helper("eav")->__("Sale"),
+                    "value" =>  'sale'
+                ),
+                array(
+                    "label" => Mage::helper("eav")->__("Promotion"),
+                    "value" =>  'promotion'
+                )
+
+            );
+        }
+        return $this->_options;
+    }
+}
